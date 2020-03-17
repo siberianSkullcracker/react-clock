7c73688af9026eb0b22bb687533c9be9da8b3cec
Daria Katysheva <dariakatysheva92@gmail.com>
2020-03-17 18:58:44 +0100

    Display time in document title

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
+const getTimeString = dateObj => {
+  return `${dateObj.getHours()}:${dateObj.getMinutes()}:${dateObj.getSeconds()}`;
+}
+
 const Clock = () => {
   const [time, setTime] = useState(new Date());
   const handleClick = () => setTime(new Date());
@@ -10,10 +14,14 @@ const Clock = () => {
     return () => clearInterval(intervalId);
   }, []);
 
+  useEffect(() => {
+    document.title = `Clock - ${getTimeString(time)}`;
+  }, [time]);
+
   return (
     <>
       <h1>Current Time</h1>
-      <h2>{time.getHours()}:{time.getMinutes()}:{time.getSeconds()}</h2>
+      <h2>{getTimeString(time)}</h2>
     </>
   )
 };
